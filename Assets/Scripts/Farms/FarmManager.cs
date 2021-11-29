f00199bbf9cd635a39fff9f726925bd2522c373e
Narmalone <62171923+Narmalone@users.noreply.github.com>
2021-11-29 18:10:56 +0100

    léger test
    test des modifs du script unity sur github

--- Assets/Scripts/Farms/FarmManager.cs
+++ Assets/Scripts/Farms/FarmManager.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class FarmManager : MonoBehaviour
 {
 
-
+    //initialiser le start
     public void Start()
     {
         UpdateFarmLevel();
     }
 
-
+    //update le text à modifier !
     public void UpdateFarmLevel()
     {
         GetComponent<TextMeshProUGUI>().text = FindObjectOfType<Farm>().CurrentFarmLevel.ToString();
